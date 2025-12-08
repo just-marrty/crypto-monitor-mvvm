70c97a725c33f2db1ce9b0b22d228d31a98299ee
Martin <marrty.h.1992@gmail.com>
2025-12-08 08:38:34 +0100

    Initial Commit

---
+++ CryptoMonitor/CryptoMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMonitorApp.swift
+//  CryptoMonitor
+//
+//  Created by Martin Hrbáček on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
